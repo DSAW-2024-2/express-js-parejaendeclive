3cb19f7993c4e6f976db7d7c58f35bbe8f65749a
Samuel Rodriguez <samuelroul@unisabana.edu.co>
2024-09-12 11:39:11 -0500

    correccion-info

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const port = 3000;
 
 // Datos del estudiante
 const student1 = {
-    name: "Samuel Andres",
-    lastName: "Rodriguez Ulloa",
-    email: "samuelroul@unisabana.edu.co",
-    id: "0000296433"
+    "name": "Samuel Andres",
+    "lastName": "Rodriguez Ulloa",
+    "email": "samuelroul@unisabana.edu.co",
+    "id": "0000296433"
 };
 
 const student2 = {
-    name: "Andrea Julieth",
-    lastName: "Sosa Rodriguez",
-    email: "andreasorol@unisabana.edu.co",
-    id: ""
+    "name": "Andrea Julieth",
+    "lastName": "Sosa Rodriguez",
+    "email": "andreasorol@unisabana.edu.co",
+    "id": "0000298246"
 };
