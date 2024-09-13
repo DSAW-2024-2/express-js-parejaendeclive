a4d783df2b26366a7232bf5282048e485ef71c11
AndreSosa21 <andrejulieth22@gmail.com>
2024-09-12 21:46:21 -0500

    endpoint-without validations

---
+++ server.js
@@ -0,0 +1,47 @@
+const express =require('express');
+const app = express();
+const port = 3000;
+// data students
+let students_data= {
+    1:{
+    name: "Samuel Andres",
+    lastName: "Rodriguez Ulloa",
+    email: "samuelroul@unisabana.edu.co",
+    id: "0000296433"
+    },
+
+    2:{
+        name: "Andrea Julieth",
+        lastName: "Sosa Rodriguez",
+        email: "andreasorol@unisabana.edu.co",
+        id:"0000298246"
+    
+    }   
+};
+//endpoint_function
+app.get('/user-info/:id', (req, res) => {
+    const id= req.params.id;
+    let selected_student=students_data[id];
+    
+        if(selected_student){
+            //call validation function
+            res.json(selected_student); 
+            
+        }
+        else{
+            res.status(404).json({ error: "Estudiante no encontrado" });
+        }
+    
+    
+    
+})
+app.listen(port, () => {
+    console.log(`server listen in http://localhost:${port}`);
+});
+
+//validation
+
+
+
+
+
